889492392ed20c5cf7d547ba335b6d97cb56c866
Sam C <samclymer@gmail.com>
2018-12-28 16:14:12 -0500

    worked on form.html in regards to the animation for form

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
   $(".sidenav").sidenav();
   $('.carousel').carousel();
+  $('.collapsible').collapsible();
+  $( ".bottom-form" ).click(function() {
+    $( ".bottom-form" ).animate({
+      left: "+=50",
+      width: "300px",
+    }, 1000, function() {
+      // Animation complete.
+    });
+  });
 });
