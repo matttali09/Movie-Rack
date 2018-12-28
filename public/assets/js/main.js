8c879a0963f63b459f386cef8a5d2a1129aed07d
Sam C <samclymer@gmail.com>
2018-12-28 16:42:18 -0500

    added in AJAX post request for users and movie ratings

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -11,3 +11,35 @@ $(document).ready(function() {
     });
   });
 });
+
+var newRating = {
+  range: $("test5").val().trim()
+};
+
+// Send the POST request.
+$.ajax("/api/movies", {
+  type: "POST",
+  data: newReview
+}).then(
+  function() {
+    console.log("created new movie rating");
+    // Reload the page to get the updated list
+    location.reload();
+  }
+);
+
+var newUser = {
+  name: $("name").val().trim(),
+  age: $("age").val().trim()
+};
+
+$.ajax("/api/users", {
+  type: "POST",
+  data: newUser
+}).then(
+  function() {
+    console.log("created new user");
+    // Reload the page to get the updated list
+    location.reload();
+  }
+);
