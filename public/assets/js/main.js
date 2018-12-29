e3a18ee227731023c06acfa9a441e5908eecba4b
Sam C <samclymer@gmail.com>
2018-12-29 12:15:41 -0500

    working on an onClick form for the movie posters

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
   $(".sidenav").sidenav();
   $('.carousel').carousel();
-  $('.collapsible').collapsible();
-  $( ".bottom-form" ).click(function() {
-    $( ".bottom-form" ).animate({
-      left: "+=50",
-      width: "300px",
-    }, 1000, function() {
-      // Animation complete.
+  $("#carouselClick").click(function(){
+    $("#movieFormAquaman").animate({
+      opacity: '0.8',
+      height: '200px',
+      width: '700px'
     });
   });
 });
