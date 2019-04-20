23a1964796bf7f2917768be099b713b9bd440dd8
matttali09 <matttaliancich09@gmail.com>
2019-04-20 19:23:00 -0400

    updated sequelize dpendency and handlebars and a few comment changes

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.get("/api/users", function (req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
+    // In this case, just db.User
     db.User.findAll({
       include: [db.Movie]
     }).then(function (dbUser) {
@@ -17,7 +17,7 @@ module.exports = function (app) {
   app.get("/api/users/:id", function (req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
+    // In this case, just db.User
     db.User.findOne({
       where: {
         id: req.params.id
